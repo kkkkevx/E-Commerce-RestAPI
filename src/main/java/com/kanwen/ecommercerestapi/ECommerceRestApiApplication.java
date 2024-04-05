0bf30dccecd467c2cfa9d9da996b75ef541a11cc
kkkkevx <xuk14@miamioh.edu>
2024-04-05 16:55:56 -0700

    Initial commit

---
+++ src/main/java/com/kanwen/ecommercerestapi/ECommerceRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.kanwen.ecommercerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceRestApiApplication.class, args);
+	}
+
+}
